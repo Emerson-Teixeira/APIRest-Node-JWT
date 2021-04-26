e771efb5a39b518f2e0039a2baad1192e4dc2eea
Emerson Teixeira <emejunior99@gmail.com>
2021-04-26 14:51:42 -0300

    Basic Auth working

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const morgan = require('morgan')
 const PORT = 3000
 const hostname = 'localhost'
 const mongoose = require('mongoose')
-const Dishes = require('./models/dishes')
 const url = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(url)
 const app = express()
@@ -15,6 +14,25 @@ connect.then((db)=>{
 
 app.use(express.json())
 app.use(morgan('dev'))
+
+function auth(req,res,next){
+    var authHeader = req.headers.authorization
+    if(!authHeader){
+        res.setHeader('WWW-Authenticate','Basic')
+        return res.status(401).json({msg:'You didn\'t pass credentials, please fix your header and try again'})
+    }
+
+    var auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+    if(auth[0] === 'admin' && auth[1] === 'password'){
+        next()
+    }
+    else{
+        res.setHeader('WWW-Authenticate','Basic')
+        return res.status(401).json({msg:'Wrong credentials! Try again'})
+    }
+}
+
+app.use(auth)
 app.use('/dishes',require('./routes/dishRouter'))
 app.use('/promotions',require('./routes/promoRouter'))
 app.use('/leaders',require('./routes/leaderRouter'))
